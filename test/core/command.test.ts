141f84b4abb44c2403da7b02171c97f569069050
ppc <ppc.develop@gmail.com>
2021-05-22 00:24:26 +0900

    Add a test case for command's pattern.

--- test/core/command.test.ts
+++ test/core/command.test.ts
@@ -1,5 +1,24 @@
+import { DirectoryEntry, FileEntry } from "../../src/core/command";
+
+
 describe('', ()=> {
     it('shoud do this thing', () => {
+        const workspaceDir: DirectoryEntry = new DirectoryEntry("workspace");
+        const compositeDir: DirectoryEntry = new DirectoryEntry("composite");
+        const testDir1: DirectoryEntry = new DirectoryEntry("test1");
+        const testDir2: DirectoryEntry = new DirectoryEntry("test2");
+        workspaceDir.add(compositeDir);
+        workspaceDir.add(testDir1);
+        workspaceDir.add(testDir2);
 
+        const directory: FileEntry = new FileEntry("Directory.ts");
+        const entity: FileEntry = new FileEntry("Entity.ts");
+        const file: FileEntry = new FileEntry("File.ts");
+        const main: FileEntry = new FileEntry("main.ts");
+        compositeDir.add(directory);
+        compositeDir.add(entity);
+        compositeDir.add(file);
+        compositeDir.add(main);
+        workspaceDir.printList();
     });
 });
