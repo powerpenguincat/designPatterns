2d6e769def640b8a3fd86edd1483bb6dab90ee33
ppc <ppc.develop@gmail.com>
2021-05-23 13:47:36 +0900

    Change a test case for test cases of all.

--- test/core/command.test.ts
+++ test/core/command.test.ts
@@ -3,24 +3,25 @@ import { JFrame } from "../../src/core/command/JFrame";
 import { JButton } from "../../src/core/command/JButton";
 import { Box } from "../../src/core/command/Box";
 
-describe('', ()=> {
-    it('shoud do this thing', () => {
-        const frame: JFrame = new JFrame("title");
-        const history: MacroCommand = new MacroCommand();
-        const canvas: DrawCanvas = new DrawCanvas(400, 400, history);
-        const clearButton: JButton = new JButton("clear");
+describe('１個の「もの」として表現する', ()=> {
+    const frame: JFrame = new JFrame("title");
+    const history: MacroCommand = new MacroCommand();
+    const canvas: DrawCanvas = new DrawCanvas(400, 400, history);
+    const clearButton: JButton = new JButton("clear");
 
-        frame.addWindowListener(frame);
-        canvas.addMouseMotionListener(frame);
+    frame.addWindowListener(frame);
+    canvas.addMouseMotionListener(frame);
 
-        const buttonBox: Box = new Box("X_AXIS");
-        buttonBox.add(clearButton);
-        const mainBox: Box = new Box("Y_AXIS");
-        mainBox.add(buttonBox);
-        mainBox.add(canvas);
-        frame.getContentPane().add(mainBox);
+    const buttonBox: Box = new Box("X_AXIS");
+    buttonBox.add(clearButton);
+    const mainBox: Box = new Box("Y_AXIS");
+    mainBox.add(buttonBox);
+    mainBox.add(canvas);
+    frame.getContentPane().add(mainBox);
 
-        frame.pack();
+    frame.pack();
+
+    it('is if you show the frame.', () => {
         frame.show();
     });
 });
