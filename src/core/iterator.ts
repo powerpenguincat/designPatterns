09242b344dffd236918df49c4da3c35ca035b84d
ppc <ppc.develop@gmail.com>
2021-05-25 22:04:43 +0900

    Add the iterator pattern.

---
+++ src/core/iterator.ts
@@ -0,0 +1,56 @@
+export interface Aggregate {
+    iterator(): Iterator;
+}
+
+export interface Iterator {
+    hasNext(): boolean;
+    next(): Object;
+}
+
+export class Book {
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getName = (): string => this.name;
+}
+
+export class BookShelf implements Aggregate {
+    private books: Book[];
+    private last: number = 0;
+
+    constructor(maxsize: number) {
+        this.books = [...Array(maxsize)].map(x => new Book(x));
+    }
+
+    getBookAt = (index: number): Book => this.books[index];
+
+    appendBook = (book: Book): void => {
+        this.books[this.last] = book;
+        this.last++;
+    }
+    
+    getLength = (): number => this.last;
+
+    iterator = (): Iterator => new BookShelfIterator(this);
+}
+
+export class BookShelfIterator implements Iterator {
+    private bookshelf: BookShelf;
+    private index: number;
+
+    constructor(bookShelf: BookShelf) {
+        this.bookshelf = bookShelf;
+        this.index = 0;
+    }
+
+    hasNext = (): boolean => this.index < this.bookshelf.getLength();
+
+    next = (): Object  => {
+        const book: Book = this.bookshelf.getBookAt(this.index);
+        this.index++;
+        return book;
+    }
+}
--- test/core/iterator.test.ts
+++ test/core/iterator.test.ts
@@ -1,4 +1,16 @@
-describe('', ()=> {
-    it('shoud do this thing', () => {
+import { BookShelf, Book, Iterator } from "../../src/core/iterator";
+
+describe('１つ１つ数え上げる', ()=> {
+    it('is if you count four books.', () => {
+        const bookShelf: BookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("Around the World in 80 Days"));
+        bookShelf.appendBook(new Book("Bible"));
+        bookShelf.appendBook(new Book("Cinderella"));
+        bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+        const it: Iterator = bookShelf.iterator();
+        while (it.hasNext()) {
+            const book: Book = it.next() as Book;
+            console.log(book.getName());
+        }
     });
 });
