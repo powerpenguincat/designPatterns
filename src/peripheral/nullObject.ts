e31fb32bb470a74a564db466d04661ca742faa79
ppc <ppc.develop@gmail.com>
2021-05-26 20:16:58 +0900

    Update nullObject to import an interface.

--- src/peripheral/nullObject.ts
+++ src/peripheral/nullObject.ts
@@ -1,11 +1,15 @@
-class Dog {
+export interface Animal {
+    sound(): string;
+}
+
+class Dog implements Animal {
     sound = (): string => 'bark';
 }
 
-class NullAnimal {
+class NullAnimal implements Animal {
     sound = (): string => null;
 }
 
-export const getAnimal = (type) => {
+export const getAnimal = (type: string): Animal => {
     return type === 'dog' ? new Dog() : new NullAnimal();
 };
--- test/peripheral/nullObject.test.ts
+++ test/peripheral/nullObject.test.ts
@@ -2,15 +2,15 @@ import { getAnimal } from "../../src/peripheral/nullObject";
 
 describe("未知の選択が行われても同じように動かす", ()=> {
     it("is if you choose an animal that is on the list.", () => {
-        const dogSound = getAnimal("dog").sound();
+        const dogSound: string = getAnimal("dog").sound();
         expect(dogSound).toBe('bark');
     });
     it("is if you choose an animal that is not on the list.", () => {
-        const catSound = getAnimal("cat").sound();
+        const catSound: string = getAnimal("cat").sound();
         expect(catSound).toBe(null);
     });
     it("is if you have not chosen an animal.", () => {
-        const nullSound = getAnimal(null).sound();
+        const nullSound: string = getAnimal(null).sound();
         expect(nullSound).toBe(null);
     });
 });
