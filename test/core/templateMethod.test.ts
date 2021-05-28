c61db149963ecd6ee36c756d13e0b52ef3e59aab
ppc <ppc.develop@gmail.com>
2021-05-28 22:19:39 +0900

    Add a test for template method.

--- test/core/templateMethod.test.ts
+++ test/core/templateMethod.test.ts
@@ -1,4 +1,13 @@
+import { AbstractDisplay, CharDisplay, StringDisplay } from "../../src/core/templateMethod";
+
 describe('', ()=> {
     it('shoud do this thing', () => {
+        const d1: AbstractDisplay = new CharDisplay("H");
+        const d2: AbstractDisplay = new StringDisplay("Hello, world.");
+        const d3: AbstractDisplay = new StringDisplay("こんにちは。");
+
+        d1.display();
+        d2.display();
+        d3.display();
     });
 });
