8b31234b33bd6a0335b530e158959420d19c14db
ppc <ppc.develop@gmail.com>
2021-05-30 00:29:41 +0900

    Add a builder pattern test.

--- test/creational/builder.test.ts
+++ test/creational/builder.test.ts
@@ -1,4 +1,18 @@
+import { TextBuilder, Director, HTMLBuilder } from "../../src/creational/builder";
+
 describe('', ()=> {
-    it('shoud do this thing', () => {
+    it('is if you want to output a plain text.', () => {
+        const textbuilder: TextBuilder = new TextBuilder();
+        const director: Director = new Director(textbuilder);
+        director.construct();
+        const result: string = textbuilder.getResult();
+        console.log(result);
+    });
+    it('is if you want to output a htmnl file.', () => {
+        const htmlbuilder: HTMLBuilder = new HTMLBuilder();
+        const director: Director = new Director(htmlbuilder);
+        director.construct();
+        const filename: string = htmlbuilder.getResult();
+        console.log(`${filename}が作成されました。`);
     });
 });
