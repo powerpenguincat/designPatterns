4e2f46d889d6f3b5d628a8727fa4aeb4fc021c5f
ppc <ppc.develop@gmail.com>
2021-06-04 21:29:36 +0900

    Add a test comment.

--- test/peripheral/decorator.test.ts
+++ test/peripheral/decorator.test.ts
@@ -1,7 +1,7 @@
 import { Display, SideBorder, FullBorder, StringDisplay } from "../../src/designPattern/peripheral/decorator";
 
-describe('', ()=> {
-    it('shoud do this thing', () => {
+describe('飾り枠と中身を同一視する', ()=> {
+    it('is if you decorate a comment', () => {
         const b1: Display = new StringDisplay("Hello, world.");
         const b2: Display = new SideBorder(b1, "#");
         const b3: Display = new FullBorder(b2);
