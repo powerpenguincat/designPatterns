836c93b2412517cab03975f9bb79094563e69d5c
ppc <ppc.develop@gmail.com>
2021-06-09 23:35:29 +0900

    Add a test code for a mediator pattern.

--- test/peripheral/mediator.test.ts
+++ test/peripheral/mediator.test.ts
@@ -1,4 +1,7 @@
-describe('', ()=> {
-    it('shoud do this thing', () => {
+import { LoginFrame } from "../../src/designPattern/peripheral/mediator";
+
+describe('GUIアプリケーションむき', ()=> {
+    it('is if you make a frame.', () => {
+        new LoginFrame("Mediator Sample");
     });
 });
