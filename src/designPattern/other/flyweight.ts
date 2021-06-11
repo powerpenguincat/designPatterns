7d09f6f4dd60693ae72de28493889d917880707a
ppc <ppc.develop@gmail.com>
2021-06-11 23:33:30 +0900

    Add a flyweight pattern.

---
+++ src/designPattern/other/flyweight.ts
@@ -0,0 +1,30 @@
+export class BigChar {
+    private charname: string;
+    private fontdata: string;
+
+    constructor(charname: string) {
+        this.charname = charname;
+    }
+
+    print = (): void => {
+        console.log(this.fontdata);
+    }
+}
+
+export class BigCharFactory {
+    private constructor() {
+    }
+}
+
+export class BigString {
+    private bigchars: BigChar[];
+
+    constructor(string: string) {
+    }
+
+    print = (): void => {
+        this.bigchars.forEach(bigchar => {
+            console.log(bigchar);
+        })
+    }
+}
