0835be626ce3153787a2b0e5f69ef872443ff33e
ppc <ppc.develop@gmail.com>
2021-06-13 23:50:10 +0900

    Add a test case in a flyweight pattern.

--- test/other/flyweight.test.ts
+++ test/other/flyweight.test.ts
@@ -1,4 +1,8 @@
-describe('', ()=> {
-    it('shoud do this thing', () => {
+import { BigString } from "../../src/designPattern/other/flyweight";
+
+describe('同じものを共有して無駄をなくす', ()=> {
+    it('is if you show the word.', () => {
+        const bs: BigString = new BigString("hoge");
+        bs.print();
     });
 });
