aa60bdca4d9033740f81ccbbb48be735c9540aa4
ppc <ppc.develop@gmail.com>
2021-06-20 23:37:51 +0900

    Add a interpreter's test case.

--- test/other/interpreter.test.ts
+++ test/other/interpreter.test.ts
@@ -1,4 +1,14 @@
-describe('', ()=> {
-    it('shoud do this thing', () => {
+import { ProgramNode, Context, Node } from "../../src/designPattern/other/interpreter";
+
+describe('インタプリタ', ()=> {
+    it('is if you parse a text.', () => {
+        try {
+            const text: string = "hoge";
+            console.log(`text = "${text}"`);
+            const node: Node = new ProgramNode();
+            node.parse(new Context(text));
+            console.log(`node = ${node}`)
+        } catch (e) {
+        }
     });
 });
