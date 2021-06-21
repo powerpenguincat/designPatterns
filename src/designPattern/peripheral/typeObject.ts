ba34646a6c048f1513501e1d4771f46c5bfa59c6
ppc <ppc.develop@gmail.com>
2021-06-21 23:01:34 +0900

    Update the typeObject pattern.

--- src/designPattern/peripheral/typeObject.ts
+++ src/designPattern/peripheral/typeObject.ts
@@ -1,10 +1,27 @@
 export class Monster {
+    private health: number;
+
     constructor(startingHealth: number) {
+        this.health = startingHealth;
     }
+
+    getAttack = (): string => "";
 }
 
 export class Dragon extends Monster {   
+
+    constructor() {
+        super(230)
+    }
+
+    getAttack = (): string => "The dragon breathes fire!";
 }
 
 export class Troll extends Monster {
+
+    constructor() {
+        super(48)
+    }
+
+    getAttack = (): string => "The troll clubs you!";
 }
