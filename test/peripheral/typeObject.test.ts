3e1a56e8e534ad473bab98fff2a8a806a13be8d9
ppc <ppc.develop@gmail.com>
2021-06-22 23:44:59 +0900

    Add the typeObject test case.

--- test/peripheral/typeObject.test.ts
+++ test/peripheral/typeObject.test.ts
@@ -1,4 +1,12 @@
-describe('', ()=> {
-    it('shoud do this thing', () => {
+import { Dragon, Troll } from "../../src/designPattern/peripheral/typeObject";
+
+describe('種族などを表現する', ()=> {
+    it('is if you summon a dragon.', () => {
+        const dragon = new Dragon();
+        expect(dragon.getAttack()).toBe("The dragon breathes fire!");
     });
+    it('is if you summon a troll.', () => {
+        const troll = new Troll();
+        expect(troll.getAttack()).toBe("The troll clubs you!");
+    })
 });
