942bffbedddc22efe624c23ecaf542bd9991094e
ppc <ppc.develop@gmail.com>
2021-06-23 23:52:06 +0900

    Add the abstractFactory pattern.

---
+++ src/designPattern/peripheral/abstractFactory.ts
@@ -0,0 +1,65 @@
+import { Writer } from "../../object/Writer";
+import { FileWriter } from "../../object/FileWriter";
+
+export abstract class Item {
+    captioin: string;
+
+    constructor(caption: string) {
+        this.captioin = caption;
+    }
+
+    abstract makeHTML(): string;
+}
+
+export abstract class Link extends Item {
+    url: string;
+
+    constructor(caption: string, url: string) {
+        super(caption);
+        this.url = url;
+    }
+}
+
+export abstract class Tray extends Item {
+    tray: Item[] = [];
+    
+    constructor(caption: string) {
+        super(caption);
+    }
+
+    add = (item: Item) => {
+        this.tray.push(item);
+    }
+}
+
+export abstract class Page {
+    title: string;
+    author: string;
+    content: [] = [];
+
+    constructor(title: string, author: string) {
+        this.title = title;
+        this.author = author;
+    }
+
+    output = (): void => {
+        try {
+            const filename: string = `${this.title}.html`;
+            const writer: Writer = new FileWriter(filename);
+            writer.close();
+            console.log(`${filename} を作成しました。`);
+        } catch (e) {
+        }
+    }
+    
+    abstract makeHTML(): string;
+}
+
+export abstract class Factory {
+    static getFactory = (classname: string): void => {
+    }
+
+    abstract createLink(caption: string, url: string): Link;
+    abstract createTray(caption: string): Tray;
+    abstract createPage(title: string, author: string): Page;
+}
