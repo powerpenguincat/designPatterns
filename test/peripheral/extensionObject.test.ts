565335e6e490d56956692c07de3fe1682b8c46fc
ppc <ppc.develop@gmail.com>
2021-06-26 22:49:28 +0900

    Add the test code for the extensionObject pattern.

--- test/peripheral/extensionObject.test.ts
+++ test/peripheral/extensionObject.test.ts
@@ -1,4 +1,9 @@
+import { Subject, ConcreteSubject, Extension } from "../../src/designPattern/peripheral/extensionObject";
+
 describe('', ()=> {
     it('shoud do this thing', () => {
+        const subject: Subject = new ConcreteSubject();
+        const extension: Extension = subject.getExteinsion("some type");
+        extension.doSomething();
     });
 });
